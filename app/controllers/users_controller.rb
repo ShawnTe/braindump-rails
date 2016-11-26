6b20bb95718747261cc0f73981c19cf91531fc86
shawn <shawn@naturalprofessional.com>
2016-11-26 14:23:13 -0800

    Add edit form, convert form to partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
