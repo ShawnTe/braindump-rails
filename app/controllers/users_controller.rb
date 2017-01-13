ee218edc659a3a72cd79039b1fba89e03585befd
shawn <shawn@naturalprofessional.com>
2017-01-12 20:28:38 -0800

    Add private logged_in_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def index
   end
 
@@ -11,7 +13,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    p '*' * 40
     @user = User.new(user_params)
     if @user.save
       login @user
@@ -28,8 +29,8 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update_attributes(user_params)
-    if @user.save
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated!"
       redirect_to @user
     else
       render 'edit'
@@ -42,5 +43,12 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
