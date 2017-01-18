13798b1303c990032a9189b27a47434a3b68e483
shawn <shawn@naturalprofessional.com>
2017-01-17 17:35:30 -0800

    Add new post form to profile page of logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
     @count = @microposts.count
+    @micropost = @user.microposts.build
   end
 
   def new
